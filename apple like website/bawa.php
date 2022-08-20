4b4c33dd37c6a9f83a739097a32bbde045798194
WHK <102664312+MrHK10@users.noreply.github.com>
2022-08-20 10:01:35 +0530

    Add files via upload

---
+++ apple like website/bawa.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>DEEP</title>
+</head>
+<body>
+<h1>ABCD</H1>
+<h2>abcd</h2>
+<h3>abcd</h3>
+<h4>abcd</h4>
+<h5>abcd</h5>
+<img src="koala.jpg" height="500px" width="400px" alt="don't show due to slow internet">
